bc5aa47530d8c3c4aa517d4bc0e0602a7838652e
Michaela Trčková <misa.trckova@volny.cz>
2023-04-24 13:41:53 +0200

    almost done

---
+++ Dice/dice.js
@@ -0,0 +1,20 @@
+export const Dice = (props) => {
+  const { side } = props;
+
+  const roll = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
+  const diceElm = document.createElement('div');
+  diceElm.classList.add('dice');
+  diceElm.innerHTML = `
+  <div class="dice__side dice__side--${side}"></div>
+  <button class="btn btn--small roll-btn">hodit</button>
+`;
+
+  diceElm.querySelector('.roll-btn').addEventListener('click', () => {
+    diceElm.replaceWith(Dice({ side: roll() }));
+  });
+
+  return diceElm;
+};
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const roll = () => {
-  return Math.floor(Math.random() * 6) + 1;
-};
+import { Dice } from './Dice/dice.js';
 
 console.log('funguju!');
+
+const diceRowElm = document.querySelector('#dice-row');
+diceRowElm.append(Dice({}));
+diceRowElm.append(Dice({}));
+diceRowElm.append(Dice({}));
+diceRowElm.append(Dice({}));
